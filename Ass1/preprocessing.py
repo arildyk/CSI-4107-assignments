9776784f97d8fbf2538b3c13e212a7c665dfbd75
Arild <65564205+arildyk@users.noreply.github.com>
2024-02-11 12:55:07 -0500

    Update preprocessing.py

--- Ass1/preprocessing.py
+++ Ass1/preprocessing.py
@@ -10,7 +10,6 @@ import time
 
 def read_file_and_tokenize(file, tag_name):
     # Extract content within the specified tag
-    #* Extraire le contenu dans le tag spécifiée
     pattern = f'<{tag_name}>(.*?)</{tag_name}>'
     matches = re.findall(pattern, file, re.DOTALL)
 
@@ -21,15 +20,12 @@ def read_file_and_tokenize(file, tag_name):
     
     for text in matches:
         # Remove HTML tags.
-        # *Supprimez les tags HTML.
         html_free_content = re.sub(r"<[^>]+>", " ", text)
 
         # Remove punctuation and numbers, replace with space.
-        #* Supprimez la ponctuation et les chiffres, remplacez-les par un espace.
         polished_text = re.sub(r"[^a-zA-Z\s]", " ", html_free_content)
 
         # Tokenize and convert to lowercase.
-        #* Tokeniser et convertir en lettres minuscules.
         tokens = nltk.word_tokenize(polished_text)
 
         for token in tokens:
@@ -39,7 +35,6 @@ def read_file_and_tokenize(file, tag_name):
 
 def stem(tokens):
     # Stem each token in tokens.
-    #* Stem chaque token dans le tableau tokens.
     p = PorterStemmer()
     stemmed_tokens = [p.stem(token) for token in tokens]
     return stemmed_tokens
@@ -71,27 +66,21 @@ if __name__ == "__main__":
             document = file.read()
 
             # Preprocess words within the TEXT tags.
-            #* Prétraitez les mots dans les tags TEXT.
             words_text = remove_stop_words(stop_words, stem(read_file_and_tokenize(document, 'TEXT')))
 
             # Preprocess words within the HEAD tags.
-            #* Prétraitez les mots dans les tags HEAD.
             words_head = remove_stop_words(stop_words, stem(read_file_and_tokenize(document, 'HEAD')))
 
             # Remove potential duplicate words by combining words_head and words_text into a set.
-            #* Supprimez les mots en double potentiels en combinant words_head et words_text dans un ensemble.
             words = set(words_head + words_text)
 
             # Sort the set by converting it back into a list.
-            #* Triez l'ensemble en le reconvertissant en liste.
             words_sorted = sorted(list(words))
 
             # Add the sorted list into vocabulary.
-            #* Ajoutez la liste triée dans le vocabulaire.
             vocabulary += words_sorted
 
-    # Remove potential duplicate words in the vocabulary and turn it to a list to sort it.
-    #* Supprimez les mots en double potentiels dans le vocabulaire et transformez-le en liste pour le trier.        
+    # Remove potential duplicate words in the vocabulary and turn it to a list to sort it.       
     vocabulary_set = set(vocabulary)
     vocabulary = sorted(list(vocabulary_set))
 
